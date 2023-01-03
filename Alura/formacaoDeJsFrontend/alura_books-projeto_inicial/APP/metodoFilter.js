a5cfd66a33eabadab2c757f44756fccc212b7583
Diogo de Assis <94648419+DiogoSis@users.noreply.github.com>
2023-01-03 11:56:11 -0300

    estudo-tipos-de-arrays

---
+++ Alura/formacaoDeJsFrontend/alura_books-projeto_inicial/APP/metodoFilter.js
@@ -0,0 +1,12 @@
+// const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront')
+// btnFiltrarLivrosFront.addEventListener('click', filtrarLivros)
+
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros(){
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+        exibirOsLivrosNaTela(livrosFiltrados)
+}
